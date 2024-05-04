75a136ce58ce4a3eaf7194e71c8cf345d56fccde
Ahmed_Ameen <ahmed.ameen@cashcall.com.eg>
2024-05-04 18:27:15 +0300

    Stack Example

--- src/main/java/com/ameen/App.java
+++ src/main/java/com/ameen/App.java
@@ -1,5 +1,7 @@
 package com.ameen;
 
+import java.util.Stack;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,24 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "In The Name of Allah 💪" );
+        System.out.println("I love Salomty ❤️");
+        
+        
+        Stack<String> myStack = new Stack<>();
+        myStack.push("Allah");
+        myStack.push("With");
+        myStack.push("Me");
+        myStack.push("Salomty");
+        
+        System.out.println(myStack);
+        myStack.pop();
+        System.out.println(myStack);
+        System.out.println(myStack.isEmpty());
+        System.out.println(myStack.search("Me"));
+        System.out.println(myStack.peek());
+        
+        
+        
     }
 }
